665f59bdb27010e736ee4a0557fc07b8e37f8ffa
iruyu <ygnk0805@outlook.jp>
2025-03-09 16:14:49 +0900

    3stime

--- src/scripts/station15-common.js
+++ src/scripts/station15-common.js
@@ -8,5 +8,24 @@ async function main() {
         elem.appendChild(li);
     })
 }
-
+async function getData() {
+    // Promiseを使って3秒後にフルネームを表示
+    const userList = [
+      { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
+      { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    ];
+    
+    // 3秒後にフルネームを作成する処理をPromiseでラップ
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const result = userList.map(user => {
+          return {
+            ...user,
+            full_name: `${user.family_name} ${user.first_name}`
+          };
+        });
+        resolve(result);
+      }, 3000);  // 3秒後に実行
+    });
+  }
 main()
